5b2326fa6afe6eb4bfbc68add53f1eb4e4ec232b
Kevin Pasquarella <pasquarella4292@gmail.com>
2017-06-14 21:25:10 -0400

    add new functions so that statements are not repeated

--- functions.py
+++ functions.py
@@ -14,3 +14,31 @@ main.show_items()
 print("\nFiltering by Pet type\n----------------------------------\n")
 
 main.filter_by_pet_type(pet_type="Dog")
+
+print("\nFiltering by Pet sub tpe\n--------------------------------\n")
+
+main.filter_by_pet_subtype(subtype="Black Lab")
+
+print("\nFiltering by color\n----------------------------------\n")
+
+main.filter_by_color(color="Yellow")
+
+print("\nFiltering by life span\n-------------------------------\n")
+
+main.filter_by_lifespan(lifespan=10)
+
+print("\nFiltering pets by age\n----------------------------\n")
+
+main.filter_by_age(2)
+
+print("\nFiltering pets by price\n--------------------------\n")
+
+main.filter_by_price(500)
+
+print("\nFiltering items by type\n-----------------------------\n")
+
+main.filter_items_by_type(item_type="Aquarium")
+
+print("\nFiltering items by price\n--------------------------------\n")
+
+main.filter_items_by_price(price=100)
--- main.py
+++ main.py
@@ -9,97 +9,100 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+def pet_columns():
+	print("ID\tType\tSub Type\tColor\tSpan\tAge\tPrice")
+
+def pet_vals(pet):
+	print(str(pet.id) + "\t" + pet.pet_type + "\t" + pet.pet_sub_type + "\t"
+			+ pet.color + "\t" + str(pet.lifespan) + "\t" + str(pet.age) + "\t" +
+			str(pet.price))
+
+def item_columns():
+	print("ID\tType\tPrice")
+
+def item_vals(item):
+	print(str(item.id) + "\t" + item.item_type + "\t" + str(item.price))
+
 def check_vals(user_input, input_type):
 	if type(user_input) != input_type:
 		raise ValueError("We're sorry, we could not read that, please try again")
 
 def show_pets():
 	pets = session.query(Pet).all()
-	print("ID\tType\tSub Type\tColor\tSpan\tAge\tPrice")
+	pet_columns()
 	for pet in pets:
-		print(str(pet.id) + "\t" + pet.pet_type + "\t" + pet.pet_sub_type + "\t"
-			+ pet.color + "\t" + str(pet.lifespan) + "\t" + str(pet.age) + "\t" +
-			str(pet.price))
+		pet_vals(pet)
 	return
 
 def show_items():
 	items = session.query(Item).all()
-	print("ID\tType\tPrice")
+	item_columns()
 	for item in items:
-		print(str(item.id) + "\t" + item.item_type + "\t" + str(item.price))
+		item_vals(item)
 	return
 
 def filter_by_pet_type(pet_type):
 	check_vals(pet_type, str)
 	pets = session.query(Pet).filter_by(pet_type=pet_type)
-	print("ID\tType\tSub Type\tColor\tSpan\tAge\tPrice")
+	pet_columns()
 	for pet in pets:
-		print(str(pet.id) + "\t" + pet.pet_type + "\t" + pet.pet_sub_type + "\t"
-			+ pet.color + "\t" + str(pet.lifespan) + "\t" + str(pet.age) + "\t" +
-			str(pet.price))
+		pet_vals(pet)
 	return
 
 def filter_by_pet_subtype(subtype):
 	check_vals(subtype, str)
 	breeds = session.query(Pet).filter_by(pet_sub_type=subtype)
-	if len(breeds) == 0:
-		print("We're sorry, we do not have any breeds of that type")
-	else:
-		print(breeds)
+	pet_columns()
+	for pet in breeds:
+		pet_vals(pet)
 	return
 
 def filter_by_color(color):
 	check_vals(color, str)
 	colors = session.query(Pet).filter_by(color=color)
-	if len(colors) == 0:
-		print("We're sorry, we are out of those colors")
-	else:
-		print(colors)
+	pet_columns()
+	for pet in colors:
+		pet_vals(pet)
 	return
 
 def filter_by_lifespan(lifespan):
 	check_vals(lifespan, int)
-	lifespans = session.query(Pet).filter_by(lifespan=filter_type)
-	if len(lifespans) == 0:
-		print("We're sorry, we are out of pets with that lifespan")
-	else:
-		print(lifespans)
+	lifespans = session.query(Pet).filter_by(lifespan=lifespan)
+	pet_columns()
+	for pet in lifespans:
+		pet_vals(pet)
 	return
 
 def filter_by_age(age):
 	check_vals(age, int)
 	ages = session.query(Pet).filter_by(age=age)
-	if len(ages) == 0:
-		print("We're sorry, we are out of pets with that age")
-	else:
-		print(ages)
+	pet_columns()
+	for pet in ages:
+		pet_vals(pet)
 	return
 
 def filter_by_price(price):
 	check_vals(price, int)
 	prices = session.query(Pet).filter_by(price=price)
-	if len(prices) == 0:
-		print("You may have to shell out some more dough")
-	else:
-		print(prices)
+	pet_columns()
+	for pet in prices:
+		pet_vals(pet)
 	return
 
 def filter_items_by_type(item_type):
 	check_vals(item_type, str)
 	items = session.query(Item).filter_by(item_type=item_type)
-	if len(items) == 0:
-		print ("No items of that type")
-	else:
-		print(items)
+	item_columns()
+	for item in items:
+		item_vals(item)
 	return
 
 def filter_items_by_price(price):
-	check_vals(price, str)
+	check_vals(price, int)
 	items = session.query(Item).filter_by(price=price)
-	if len(items) == 0:
-		print("Seriously, get a bigger wallet")
-	else:
-		print(items)
+	item_columns()
+	for item in items:
+		item_vals(item)
 	return
 
 
