48c5de9a798a547680a2993b79d185fc0f5a05ab
Kevin Pasquarella <pasquarella4292@gmail.com>
2017-06-15 20:43:29 -0400

    add check for query

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 import sys
+from sqlalchemy import exists
 
 engine = create_engine('sqlite:///petstore.db')
 Base.metadata.bind = engine
@@ -27,6 +28,10 @@ def check_vals(user_input, input_type):
 	if type(user_input) != input_type:
 		raise ValueError("We're sorry, we could not read that, please try again")
 
+def empty_query(thing):
+	if thing.count() == 0:
+		print("We're sorry, we do not have any of those. Please try another search.")
+
 def show_pets():
 	pets = session.query(Pet).all()
 	pet_columns()
@@ -44,6 +49,7 @@ def show_items():
 def filter_by_pet_type(pet_type):
 	check_vals(pet_type, str)
 	pets = session.query(Pet).filter_by(pet_type=pet_type)
+	empty_query(pets)
 	pet_columns()
 	for pet in pets:
 		pet_vals(pet)
@@ -52,6 +58,7 @@ def filter_by_pet_type(pet_type):
 def filter_by_pet_subtype(subtype):
 	check_vals(subtype, str)
 	breeds = session.query(Pet).filter_by(pet_sub_type=subtype)
+	empty_query(breeds)
 	pet_columns()
 	for pet in breeds:
 		pet_vals(pet)
@@ -60,6 +67,7 @@ def filter_by_pet_subtype(subtype):
 def filter_by_color(color):
 	check_vals(color, str)
 	colors = session.query(Pet).filter_by(color=color)
+	empty_query(colors)
 	pet_columns()
 	for pet in colors:
 		pet_vals(pet)
@@ -68,6 +76,7 @@ def filter_by_color(color):
 def filter_by_lifespan(lifespan):
 	check_vals(lifespan, int)
 	lifespans = session.query(Pet).filter_by(lifespan=lifespan)
+	empty_query(lifespans)
 	pet_columns()
 	for pet in lifespans:
 		pet_vals(pet)
@@ -76,6 +85,7 @@ def filter_by_lifespan(lifespan):
 def filter_by_age(age):
 	check_vals(age, int)
 	ages = session.query(Pet).filter_by(age=age)
+	empty_query(ages)
 	pet_columns()
 	for pet in ages:
 		pet_vals(pet)
@@ -84,6 +94,7 @@ def filter_by_age(age):
 def filter_by_price(price):
 	check_vals(price, int)
 	prices = session.query(Pet).filter_by(price=price)
+	empty_query(prices)
 	pet_columns()
 	for pet in prices:
 		pet_vals(pet)
@@ -92,6 +103,7 @@ def filter_by_price(price):
 def filter_items_by_type(item_type):
 	check_vals(item_type, str)
 	items = session.query(Item).filter_by(item_type=item_type)
+	empty_query(items)
 	item_columns()
 	for item in items:
 		item_vals(item)
@@ -100,19 +112,12 @@ def filter_items_by_type(item_type):
 def filter_items_by_price(price):
 	check_vals(price, int)
 	items = session.query(Item).filter_by(price=price)
+	empty_query(items)
 	item_columns()
 	for item in items:
 		item_vals(item)
 	return
 
-'''def discounted():
-	pets = session.query(Pet).all()
-	for pet in pets:
-		if pet.age >= (pet.lifespan/2):
-			new_price = pet.price*0.75
-			print(str(pet.id) + "\t" + pet.pet_type + "\t" + pet.pet_sub_type + "\t"
-			+ pet.color + "\t" + str(pet.lifespan) + "\t" + str(pet.age) + "\t" +
-			str(pet.new_price))'''
 
 
 
